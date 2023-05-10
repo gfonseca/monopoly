96e46a9a2d5eb70ecab7086f4dbd5ce41f457494
Georgio Barbosa <georgio.barbosa@gmial.com>
2023-05-10 19:52:04 -0300

    Add player strategy

--- entity/player.py
+++ entity/player.py
@@ -1,25 +1,62 @@
 from abc import ABC, abstractclassmethod
+import random
 
 
 class PlayerStrategy(ABC):
 
     @abstractclassmethod
-    def makeDecision(self):
+    def make_decision(self, rent_value: int, player_balance: int) -> bool:
+        pass
+
+
+class RandomStrategy(PlayerStrategy):
+
+    def make_decision(self, rent_value: int, player_balance: int) -> bool:
+        if random.randint(1, 10) > 5:
+            return True
+
+        return False
+
+
+class ImpulsiveStrategy(PlayerStrategy):
+
+    def make_decision(self, rent_value: int, player_balance: int) -> bool:
+        return True
+
+
+class DemandingStrategy(PlayerStrategy):
+
+    def make_decision(self, rent_value: int, player_balance: int) -> bool:
+        if rent_value > 50:
+            return True
+        return False
+
+
+class CautiousStrategy(PlayerStrategy):
+
+    def make_decision(self, rent_value: int, player_balance: int) -> bool:
         pass
 
 
 class Player:
 
     def __init__(self, name: str, strategy: PlayerStrategy):
-        self.name: str = name
         self.strategy: PlayerStrategy = strategy
+        self.name: str = name
         self.bank: int = 0
 
-    def is_broke(self):
+    def is_broke(self) -> bool:
         return self.bank < 0
 
-    def receiveMoney(self, amount: int):
+    def receive_money(self, amount: int):
         self.bank += amount
+
+    def pay_money(self, amount: int):
+        self.bank -= amount
+
+    def make_decision(self, rent_value: int) -> bool:
+        return self.strategy.make_decision(rent_value, self.bank)
+
 
 # def build_player(name: str) -> Player:
 #     match strategy:
--- test/test_player.py
+++ test/test_player.py
@@ -1,13 +1,85 @@
+from unittest.mock import MagicMock
 import pytest
-import mock
+from entity.player import DemandingStrategy, Player, RandomStrategy
 
-from entity.player import Player, PlayerStrategy
+
+@pytest.fixture(scope="function")
+def mock_strategy(mocker):
+    return mocker.patch('entity.player.PlayerStrategy')
+
+
+@pytest.fixture(scope="function")
+def player_john(mock_strategy):
+    return Player(name="JohnRandom", strategy=mock_strategy)
+
+
+@pytest.fixture(scope="function")
+def player_ned(mocker):
+    return mocker.patch('entity.player.Player')
 
 
 class TestPlayer:
-    def test_broken(self):
-        PLAYER_NAME = "JohnRandom"
-        stg = mock.Mock(PlayerStrategy)
-        p = Player(name=PLAYER_NAME, strategy=stg)
 
-        assert p.is_broke() == False
+    def test_not_start_broken(self, player_john):
+        assert not player_john.is_broke()
+
+    def test_broken_player(self, player_john):
+        player_john.bank = -1
+
+        assert player_john.is_broke()
+
+    def test_receive_money(self, player_john):
+        player_john.receive_money(300)
+        assert player_john.bank == 300
+
+    def test_pay_money(self, player_john):
+        player_john.bank = 400
+        player_john.pay_money(400)
+
+        assert player_john.bank == 0
+
+    def test_strategy(self, mocker, mock_strategy):
+        PROPERTY_VALUE = 1500
+        mock_strategy.make_decision = MagicMock()
+        mock_strategy.make_decision.return_value = True
+        p = Player(name="JohnRandom", strategy=mock_strategy)
+        res = p.make_decision(PROPERTY_VALUE)
+
+        mock_strategy.make_decision.assert_called_once()
+        assert res
+
+
+class TestStrategy:
+
+    def test_random_strategy(self):
+        PROPERTY_VALUE = 0
+        PLAYER_BALANCE = 0
+        strategy = RandomStrategy()
+        res = strategy.make_decision(PROPERTY_VALUE, PLAYER_BALANCE)
+
+        assert isinstance(res, bool)
+
+    def test_demanding_strategy_deny(self):
+        RENT_VALUE = 10
+        PLAYER_BALANCE = 0
+        strategy = DemandingStrategy()
+        res = strategy.make_decision(RENT_VALUE, PLAYER_BALANCE)
+
+        assert res == False
+
+    def test_demanding_strategy_agree(self):
+        RENT_VALUE = 51
+        PLAYER_BALANCE = 0
+        strategy = DemandingStrategy()
+        res = strategy.make_decision(RENT_VALUE, PLAYER_BALANCE)
+
+        assert res
+
+    def test_demanding_strategy_agree(self):
+        RENT_VALUE = 51
+        PLAYER_BALANCE = 100
+
+        strategy = DemandingStrategy()
+        res = strategy.make_decision(RENT_VALUE, PLAYER_BALANCE)
+
+        assert res
